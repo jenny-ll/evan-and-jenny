9fb3109591727c7f84ddff3882e1a9d587c45716
Jenny Liu <jenny.liu.418@gmail.com>
2026-09-13 20:02:58 -0700

    Fix mobile version

--- js/scripts.js
+++ js/scripts.js
@@ -153,15 +153,15 @@ $(document).ready(function () {
   /***************** Nav Transformicon ******************/
 
   /* When user clicks the Icon */
-  $(".nav-toggle").click(function () {
+  $(".nav-toggle").click(function (event) {
     $(this).toggleClass("active");
     $(".header-nav").toggleClass("open");
     event.preventDefault();
   });
   /* When user clicks a link */
-  $(".header-nav li a").click(function () {
-    $(".nav-toggle").toggleClass("active");
-    $(".header-nav").toggleClass("open");
+  $(".header-nav li a, .header-nav .language-toggle").click(function () {
+    $(".nav-toggle").removeClass("active");
+    $(".header-nav").removeClass("open");
   });
 
   /***************** Smooth Scrolling ******************/
